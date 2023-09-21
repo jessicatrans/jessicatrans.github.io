46b097df0563373326b136a722159ae829107209
jessicatrans <jgtrans@ucdavis.edu>
2023-09-20 20:14:21 -0700

    cleaning up code

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -55,11 +55,11 @@ typewriter.typeString('hello!')
     .deleteAll()
     .start();
 
-
-
-
-
-
+/* 
+========================
+    Scrolling Feature
+========================
+*/
 
 // When the user scrolls down 20px from the top of the document, slide down the navbar
 window.onscroll = function() {scrollFunction()};
@@ -79,7 +79,11 @@ function scrollFunction() {
     }
 }
 
-// Responsive Topnav
+/* 
+========================
+    Responsive Navbar
+========================
+*/
 function hamburgerIcon() {
     var x = document.getElementById("nav");
     if (x.className === "navbar") {
@@ -88,48 +92,3 @@ function hamburgerIcon() {
       x.className = "navbar";
     }
 }
-
-// filterSelection("all")
-// function filterSelection(c) {
-//   var x, i;
-//   x = document.getElementsByClassName("each-project");
-//   if (c == "all") c = "";
-//   for (i = 0; i < x.length; i++) {
-//     w3RemoveClass(x[i], "show");
-//     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-//   }
-// }
-
-// function w3AddClass(element, name) {
-//   var i, arr1, arr2;
-//   arr1 = element.className.split(" ");
-//   arr2 = name.split(" ");
-//   for (i = 0; i < arr2.length; i++) {
-//     console.log(element.className, arr2[i]);
-//     if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
-//   }
-// }
-
-// function w3RemoveClass(element, name) {
-//   var i, arr1, arr2;
-//   arr1 = element.className.split(" ");
-//   arr2 = name.split(" ");
-//   for (i = 0; i < arr2.length; i++) {
-//     while (arr1.indexOf(arr2[i]) > -1) {
-//       arr1.splice(arr1.indexOf(arr2[i]), 1);     
-//     }
-//   }
-//   element.className = arr1.join(" ");
-// }
-
-// // Add active class to the current button (highlight it)
-// var btnContainer = document.getElementById("button-section");
-// var btns = btnContainer.getElementsByClassName("filter-button");
-// for (var i = 0; i < btns.length; i++) {
-//   btns[i].addEventListener("click", function(){
-//     var current = document.getElementsByClassName("active");
-//     current[0].className = current[0].className.replace(" active", "");
-//     this.className += " active";
-//     console.log(current);
-//   });
-// }
