448e30b684cc14e21951d9d678c9cdec8e8db9e9
jessicatrans <jgtrans@ucdavis.edu>
2023-11-13 23:32:32 -0800

    fixed extra padding in art page as well as modal

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -82,35 +82,34 @@ function scrollFunction() {
     }
 }
 
-function togglePopUp() {
-    document.getElementById("popup-view").classList.toggle("active");
-}
+function togglePopUp(modalNumber) {
+    // Get the modal
+    var modal = document.getElementById("myModal");
+    
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
 
-// Get the modal
-var modal = document.getElementById("myModal");
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+    modal.style.display = "block";
+    }
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
     modal.style.display = "none";
-  }
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+    }
 }
+
 
 // if not under index.html, then keep navbar visible at all times
 // function navBar() {
