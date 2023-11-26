dd91c43d0df8fc764751f32d089db0301663d152
jessicatrans <jgtrans@ucdavis.edu>
2023-11-26 00:29:29 -0800

    update comments in js for modal

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -82,34 +82,11 @@ function scrollFunction() {
     }
 }
 
-function togglePopUp(modalNumber) {
-    // Get the modal
-    var modal = document.getElementById("myModal");
-    
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks the button, open the modal 
-    btn.onclick = function() {
-    modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-    modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-    }
-}
-
+/* 
+========================
+    Modal Pop Up Feature
+========================
+*/
 document.addEventListener('click', function (e) {
     var clickedElement = e.target;
 
