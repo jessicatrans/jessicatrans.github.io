3b8f2e2fef1843a189f95030ca77652bd4f7c29c
Jessica Trans <82259375+jessicatrans@users.noreply.github.com>
2024-12-31 23:37:55 -0800

    carousel adjustments

--- js/carousel.js
+++ js/carousel.js
@@ -9,37 +9,38 @@ const slides = document.querySelectorAll(".item");
 const button = document.querySelectorAll(".carousel-button");
 
 let current = 0;
-let prev = 4;
+let prev = 7;
 let next = 1;
 
 for (let i = 0; i < button.length; i++) {
-  button[i].addEventListener("click", () => i == 0 ? gotoPrev() : gotoNext());
+  button[i].addEventListener("click", () => (i == 0 ? gotoPrev() : gotoNext()));
 }
 
-const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
+const gotoPrev = () =>
+  current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
 
-const gotoNext = () => current < 4 ? gotoNum(current + 1) : gotoNum(0);
+const gotoNext = () => (current < 7 ? gotoNum(current + 1) : gotoNum(0));
 
-const gotoNum = number => {
+const gotoNum = (number) => {
   current = number;
   prev = current - 1;
   next = current + 1;
 
   for (let i = 0; i < slides.length; i++) {
-    slides[i].classList.remove("active");
+    slides[i].classList.remove("current");
     slides[i].classList.remove("prev");
     slides[i].classList.remove("next");
   }
 
-  if (next == 5) {
+  if (next == 8) {
     next = 0;
   }
 
   if (prev == -1) {
-    prev = 4;
+    prev = 7;
   }
 
-  slides[current].classList.add("active");
+  slides[current].classList.add("current");
   slides[prev].classList.add("prev");
   slides[next].classList.add("next");
-}
+};
